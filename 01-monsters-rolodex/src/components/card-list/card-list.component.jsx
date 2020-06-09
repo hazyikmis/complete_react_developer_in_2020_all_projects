43d1e330a525b3faa800bdff8110691f2516e73d
Halil Azyikmis <hazyi@halo-pc-hp.hacettepe.edu.tr>
2020-06-09 03:25:31 +0200

    MR: card component added

--- 01-monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ 01-monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import "./card-list.styles.css";
 
+import { Card } from "../card/card.component";
+
 export const CardList = (props) => {
   const {monsters} = props;
   return (
     <div className="card-list">
       {monsters.map((monster) => (
-        <h1 key={monster.id}>{monster.name}</h1>
+        <Card key={monster.id} monster={monster} />
       ))}
     </div>
   );
