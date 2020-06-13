3052cb23ec2dd6196e3192a6861715fa37530854
Halil Azyikmis <hazyi@halo-pc-hp.hacettepe.edu.tr>
2020-06-13 17:33:56 +0200

    KC: adding header component and links to home, shop page and contact

--- king-clothing/src/App.js
+++ king-clothing/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
 import {ShopPage} from "./pages/shop/shop.component";
+import Header from "./components/header-component/header.component"
 
 // const HatsPage = (props) => {
 //   //console.log(props);
@@ -16,6 +17,7 @@ import {ShopPage} from "./pages/shop/shop.component";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         {/* <Route exact path="/hats" render={() => <HatsPage />} /> */}
