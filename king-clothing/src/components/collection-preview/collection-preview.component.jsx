7bea571998ca4659f87f59237f9458f8fa379527
Halil Azyikmis <hazyi@halo-pc-hp.hacettepe.edu.tr>
2020-06-13 17:10:34 +0200

    KC: updating  our app with a shop page , collection preview and collection item components

--- king-clothing/src/components/collection-preview/collection-preview.component.jsx
+++ king-clothing/src/components/collection-preview/collection-preview.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "./collection-preview.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => {
   return (
@@ -9,8 +10,8 @@ const CollectionPreview = ({ title, items }) => {
       <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
-          .map((item) => (
-            <div key={item.id}>{item.name}</div>
+          .map(({id, ...otherItemProps}) => (
+            <CollectionItem key={id} {...otherItemProps} />
           ))}
       </div>
     </div>
@@ -18,3 +19,13 @@ const CollectionPreview = ({ title, items }) => {
 };
 
 export default CollectionPreview;
+
+
+/*
+{items
+  .filter((item, idx) => idx < 4)
+  .map((item) => (
+    <div key={item.id}>{item.name}</div>
+  ))}
+
+*/
